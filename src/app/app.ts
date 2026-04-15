28cc4b5dd18c8e201b2fe75eb4571eae7e502344
tejas <jshettytejas@gmail.com>
2026-04-15 04:56:24 +0000

    day0

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  //template: `<h2>hiii word</h2>`,
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('property-app');
